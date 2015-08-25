a5cb92cf110a62b5143f2d47a69c61ddc5494db1
Erik Price <github@erikprice.net>
2015-08-25 00:24:04 -0700

    hey it works

---
+++ src/index.js
@@ -0,0 +1,38 @@
+require('babel-core/polyfill')
+
+var YamahaAPI = require('yamaha-nodejs');
+var receiver = new YamahaAPI('192.168.1.222');
+var restify = require('restify');
+var fs = require("fs");
+
+let server = restify.createServer({
+  name: 'yamamahahamamahahamanamanamnamna',
+  version: '1.0.0'
+});
+
+function setVolume(req, res, next) {
+  let {body: {direction}} = req;
+  direction = direction.toLowerCase();
+
+  switch (direction.toLowerCase()) {
+    case "up": {
+      console.log("going up");
+      receiver.volumeUp(50);
+      break;
+    }
+    case "down": {
+      console.log("going down");
+      receiver.volumeDown(50);
+    }
+  }
+
+  res.send(200);
+  return next();
+}
+
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+
+server.post("/volume", setVolume);
+
+server.listen(8081, () => console.log("Listening..."));
